c3632e4f71dd130525f4c6f6a936f7a7e323f020
Bob Bass <bob@bbass.co>
2022-08-14 15:20:36 -0500

    playing with routing

--- app.vue
+++ app.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import "@/assets/css/tailwind.css";
-const config = useRuntimeConfig();
+
 // CLIENT ENV VARS
-const { CLIENT_VAR } = config.public;
+// const config = useRuntimeConfig();
+// const { CLIENT_VAR } = config.public;
 </script>
 
 <template>
   <Navbar />
-  <div class="w-full block">{{ CLIENT_VAR }}</div>
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+//
+</script>
+<template>
+  <div>First Page</div>
+</template>
+
+<style scoped></style>
